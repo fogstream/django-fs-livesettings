8126e33b75cbee9c23cd92b9b1c5f9d05d60a6c1
Yuri Lya <Yuri Lya@localhost>
2013-02-22 12:55:43 +1100

    Dict for types for fields and widgets.

--- forms.py
+++ forms.py
@@ -8,20 +8,18 @@ from . import models as _models
 
 
 class SettingAdminForm(forms.ModelForm):
+    TYPE_WIDGET = {
+        _models.Setting.TYPE_BOOLEAN: widgets.CheckboxInput(),
+        _models.Setting.TYPE_CHAR: admin_widgets.AdminTextInputWidget(),
+        _models.Setting.TYPE_DATE: admin_widgets.AdminDateWidget(),
+        _models.Setting.TYPE_EMAIL: admin_widgets.AdminTextInputWidget(),
+        _models.Setting.TYPE_INTEGER: admin_widgets.AdminIntegerFieldWidget(),
+        _models.Setting.TYPE_URL: admin_widgets.AdminURLFieldWidget(),
+    }
+
     def __init__(self, *args, **kwargs):
         super(SettingAdminForm, self).__init__(*args, **kwargs)
-        if self.instance.type == _models.Setting.TYPE_BOOLEAN:
-            self.fields['value'].widget = widgets.CheckboxInput()
-        elif self.instance.type == _models.Setting.TYPE_CHAR:
-            self.fields['value'].widget = admin_widgets.AdminTextInputWidget()
-        elif self.instance.type == _models.Setting.TYPE_DATE:
-            self.fields['value'].widget = admin_widgets.AdminDateWidget()
-        elif self.instance.type == _models.Setting.TYPE_EMAIL:
-            self.fields['value'].widget = admin_widgets.AdminTextInputWidget()
-        elif self.instance.type == _models.Setting.TYPE_INTEGER:
-            self.fields['value'].widget = admin_widgets.AdminIntegerFieldWidget()
-        elif self.instance.type == _models.Setting.TYPE_URL:
-            self.fields['value'].widget = admin_widgets.AdminURLFieldWidget()
+        self.fields['value'].widget = self.TYPE_WIDGET.get(self.instance.type, admin_widgets.AdminTextInputWidget())
 
     def clean_value(self):
         value = self.cleaned_data['value']
