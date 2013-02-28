207d93448c309db999305c68404e93200660e1e9
Yuri Lya <Yuri Lya@localhost>
2013-02-28 13:40:10 +1100

    .

--- livesettings/__init__.py
+++ livesettings/__init__.py
@@ -9,20 +9,27 @@ from . import models as _models
 class LiveSettings(object):
     def __init__(self):
         try:
-            key_list = []
-            for type_key_item in _settings.TYPE_KEY_LIST:
-                key = type_key_item[1]
-                setting, created = _models.Setting.objects.get_or_create(key=key)
-                type = type_key_item[0]
-                if setting.type != type:
-                    setting.type = type
-                    setting.save()
-                key_list.append(key)
-            _models.Setting.objects.exclude(key__in=key_list).delete()
+            _models.Setting.objects.exists()
         except DatabaseError:
-            pass
+            return
+
+        key_list = []
+        for type_key_item in _settings.TYPE_KEY_LIST:
+            key = type_key_item[1]
+            setting, created = _models.Setting.objects.get_or_create(key=key)
+            type = type_key_item[0]
+            if setting.type != type:
+                setting.type = type
+                setting.save()
+            key_list.append(key)
+        _models.Setting.objects.exclude(key__in=key_list).delete()
 
     def __getattr__(self, name):
+        try:
+            _models.Setting.objects.exists()
+        except DatabaseError:
+            return None
+
         try:
             setting = _models.Setting.objects.get(key=name)
             return setting.get_value
