d91d90d4c0319cf49d67d68a40f6f30ca5978bca
Yuri Lya <Yuri Lya@localhost>
2013-02-28 14:24:38 +1100

    .

--- livesettings/__init__.py
+++ livesettings/__init__.py
@@ -1,16 +1,23 @@
 # coding=utf-8
 
 from django.db.utils import DatabaseError
+from django.db import connections
 
 from . import settings as _settings
 from . import models as _models
 
 
+def model_installed(model):
+    opts = model._meta
+    connection = connections['default']
+    tables = connection.introspection.table_names()
+    converter = connection.introspection.table_name_converter
+    return ((converter(opts.db_table) in tables) or (opts.auto_created and converter(opts.auto_created._meta.db_table) in tables))
+
+
 class LiveSettings(object):
     def __init__(self):
-        try:
-            _models.Setting.objects.exists()
-        except DatabaseError:
+        if not model_installed(_models.Setting):
             return
 
         key_list = []
@@ -25,11 +32,6 @@ class LiveSettings(object):
         _models.Setting.objects.exclude(key__in=key_list).delete()
 
     def __getattr__(self, name):
-        try:
-            _models.Setting.objects.exists()
-        except DatabaseError:
-            return None
-
         try:
             setting = _models.Setting.objects.get(key=name)
             return setting.get_value
