6614bd67a754a823bae43a79c7158bab5f74d7be
Yuri Lya <Yuri Lya@localhost>
2013-03-09 12:46:56 +1100

    .

--- livesettings/__init__.py
+++ livesettings/__init__.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from livesettings import signals
+
 from django.db.utils import DatabaseError
 from django.db import connections
 
@@ -15,28 +17,20 @@ def model_installed(model):
     return ((converter(opts.db_table) in tables) or (opts.auto_created and converter(opts.auto_created._meta.db_table) in tables))
 
 
-class LiveSettings(object):
-    def __init__(self):
-        if not model_installed(_models.Setting):
-            return
+def init_livesettings():
+    if not model_installed(_models.Setting):
+        return
 
-        key_list = []
-        for type_key_item in _settings.TYPE_KEY_LIST:
-            key = type_key_item[1]
-            setting, created = _models.Setting.objects.get_or_create(key=key)
-            type = type_key_item[0]
-            if setting.type != type:
-                setting.type = type
-                setting.save()
-            key_list.append(key)
-        _models.Setting.objects.exclude(key__in=key_list).delete()
-
-    def __getattr__(self, name):
-        try:
-            setting = _models.Setting.objects.get(key=name)
-            return setting.get_value
-        except _models.Setting.DoesNotExist:
-            raise AttributeError('\'{0}\' object has no attribute \'{1}\''.format(type(self).__name__, name))
+    key_list = []
+    for type_key_item in _settings.TYPE_KEY_LIST:
+        key = type_key_item[1]
+        setting, created = _models.Setting.objects.get_or_create(key=key)
+        type = type_key_item[0]
+        if setting.type != type:
+            setting.type = type
+            setting.save()
+        key_list.append(key)
+    _models.Setting.objects.exclude(key__in=key_list).delete()
 
 
-livesettings = LiveSettings()
+init_livesettings()
