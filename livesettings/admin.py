59c9d58644f16b721ab6ece8fb8af74f329ee0e4
Yuri Lya <yuri-l@users.noreply.github.com>
2013-05-15 17:54:50 +1100

    .

--- livesettings/admin.py
+++ livesettings/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from livesettings import models as _models
 from livesettings import forms as _forms
+from livesettings import settings as _settings
+from livesettings import types as _types
 
 
 class SettingAdmin(admin.ModelAdmin):
@@ -21,3 +23,21 @@ class SettingAdmin(admin.ModelAdmin):
 
 
 admin.site.register(_models.Setting, SettingAdmin)
+
+
+def init_livesettings():
+    keys = []
+    for conf_item in _settings.CONF:
+        key = conf_item[0]
+        setting, created = _models.Setting.objects.get_or_create(key=key)
+        type = conf_item[1]
+        assert type in _types.TYPES
+        if setting.type != type:
+            setting.type = type
+            setting.value = None
+            setting.save()
+        keys.append(key)
+    _models.Setting.objects.exclude(key__in=keys).delete()
+
+
+init_livesettings()
