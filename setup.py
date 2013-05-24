2093c86ff2eefd3dd09b56efda12cabcff9b87df
Yuri Lya <Yuri Lya@localhost>
2013-05-24 16:33:10 +1100

    .

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-livesettings',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=['django-picklefield']
 )
