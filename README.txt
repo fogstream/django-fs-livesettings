99e130232152e0bf9a03db5e452ecdcc0c614716
Yuri Lya <yuri-l@users.noreply.github.com>
2014-05-07 14:27:59 +1100

    Prepared for uploading to PyPI.

---
+++ README.txt
@@ -0,0 +1,33 @@
+Introduction
+============
+
+django-livesettings is the Django-related reusable app provides the ability to store settings in a database and change them from an admin interface.
+
+
+Installation
+============
+
+1. Install django-livesettings using ``pip`` or ``easy_install``::
+
+    $ pip install django-livesettings
+
+2. Add the ``'livesettings'`` application to the ``INSTALLED_APPS`` setting of your Django project ``settings.py`` file::
+
+    INSTALLED_APPS = (
+        ...
+        'livesettings',
+        ...
+    )
+
+3. Add ``LIVESETTINGS_CONF`` to a ``settings.py`` file::
+
+    LIVESETTINGS_CONF = (
+        ('MY_SETTING', 'email', u'Description of this setting.'),
+    )
+
+
+Usage
+=====
+
+    from livesettings.conf import livesettings
+    print livesettings.MY_SETTING
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 # coding=utf-8
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='django-livesettings',
-    packages=find_packages(),
-    install_requires=['django-picklefield']
+    version = '0.1',
+    packages = ['livesettings'],
+    include_package_data = True,
+    install_requires = ['django-picklefield'],
+    author = 'Yuri Lya',
+    author_email = 'yuri.lya@fogstream.ru',
+    url = 'https://bitbucket.org/fogstream/django-livesettings',
+    license = 'The MIT License (MIT)',
+    description = 'The Django-related reusable app provides the ability to store settings in a database and change them from an admin interface.',
+    long_description = open('README.txt').read(),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
 )
