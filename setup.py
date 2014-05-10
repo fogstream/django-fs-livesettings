1bd27bb1b2503bfce7bc5ac9be81d222690b4a5e
Yuri Lya <Yuri Lya@localhost>
2014-05-10 18:09:58 +1100

    setup fixed.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 
 
 setup(
-    name='fogstream-livesettings',
+    name='django-fs-livesettings',
     version = '0.1',
     packages = ['livesettings'],
     include_package_data = True,
     install_requires = ['django-picklefield'],
     author = 'Yuri Lya',
     author_email = 'yuri.lya@fogstream.ru',
-    url = 'https://bitbucket.org/fogstream/fogstream-livesettings',
+    url = 'https://bitbucket.org/fogstream/django-fs-livesettings',
     license = 'The MIT License (MIT)',
-    description = 'The Django-related reusable app provides the ability to store settings in a database and change them from an admin interface.',
+    description = 'The Django-related reusable app provides the ability to store settings in a database and configure settings via an admin interface.',
     long_description = open('README.txt').read(),
     classifiers = [
         "Development Status :: 5 - Production/Stable",
